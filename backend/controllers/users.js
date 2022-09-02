010eeaf1e6ae743b5c66ab0799a9dd1e91628363
Aleksey Paramonov <paramonovav90@gmail.com>
2022-09-02 12:54:26 +0400

    test env in server_1

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -146,7 +146,7 @@ module.exports.login = (req, res, next) => {
   return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : JWT_SECRET_DEFAULT, { expiresIn: '7d' });
-      res.send({ token, NODE_ENV, JWT_SECRET });
+      res.send({ token });
     })
     .catch((err) => {
       next(err);
